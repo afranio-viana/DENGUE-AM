036234f57f1b43f8903434ddc86402a52d264b49
afranio-viana <afranio.esquerdo.viana@gmail.com>
2025-10-16 00:08:58 -0400

    Pipeline:Demografia raw-genero-faixa_etaria

--- modules/sidrapy_requests.py
+++ modules/sidrapy_requests.py
@@ -1,18 +1,21 @@
 import pandas as pd
 import sidrapy
 
-def sidrapy_populacao(nome,tabela,nivel_territorial,codigo_ibge,variavel,uf):
-    data = sidrapy.get_table(table_code=tabela,territorial_level=nivel_territorial,ibge_territorial_code=codigo_ibge,variable=variavel) 
+def sidrapy_populacao(nome,tabela,nivel_territorial,variavel,codigo_ibge,uf,colunas,classificacao):
+    data = sidrapy.get_table(table_code=tabela,territorial_level=nivel_territorial,variable=variavel,ibge_territorial_code=codigo_ibge,classifications=classificacao) 
 
     data.columns = data.iloc[0]
-    df = data.iloc[1:, [5,6,4,10]]
+    df = data.iloc[1:, colunas]
     df["ESTADO"] = df["Município"].str.split(' - ',expand=True).iloc[:, -1]
     df["MUNICIPIO"] = df["Município"].str.split(' - ',expand=True)[0]
-
+    df= df.drop('Município', axis=1)
     filtro_estado = df["ESTADO"]==uf
 
     df = df[filtro_estado]
-    df = df.iloc[:, [4,0,5,2,3]]
-    df = df.rename(columns={'Ano':'ANO','Valor':'VALOR','Município (Código)':'CODIGO'})
+    #df = df.iloc[:, [4,0,5,2,3]]
+    df = df.rename(columns={'Município (Código)':'CODIGO'})
+    for nomes in df.columns:
+        df = df.rename(columns={nomes:nomes.upper()})
 
     df.to_csv(f'data/raw/ibge/{nome}.csv')
+    print(f"\nArquivo {nome}.csv criado")
--- src/ingestion/ibge_populacao.py
+++ src/ingestion/ibge_populacao.py
@@ -1,10 +1,18 @@
-import pandas as pd
-import sidrapy
 from modules.sidrapy_requests import sidrapy_populacao
+import sidrapy
 
 
-sidrapy_populacao("populacao_2022","4709","6","all","93","AM")
-sidrapy_populacao("densidade_demografica_2022","4714","6","all","614","AM")
-#sidrapy_populacao("grupo_idade_2022","1209","3","all","1000606","AM")
+genero = {'2':'4,5'}
+idade1 = {'287':'93070,93084,93085,93086,93087,93088,93089,93090'}
+idade2 = {'287':'93091,93092,93093,93094,93095,93096,93097,93098'}
+idade3 = {'287':'49108,49109,60040,60041,6653'}
+
+
+sidrapy_populacao("populacao_2022","4709","6","93","all","AM",[5,6,4,10],None)
+sidrapy_populacao("densidade_demografica_2022","4714","6","614","all","AM",[5,6,4,10],None)
+sidrapy_populacao('genero_2022','9514','6','93','all','AM',[5,6,12,10,4],genero)
+sidrapy_populacao('faixa_etaria1_2022','9514','6','93','all','AM',[5,6,12,10,4],idade1)
+sidrapy_populacao('faixa_etaria2_2022','9514','6','93','all','AM',[5,6,12,10,4],idade2)
+sidrapy_populacao('faixa_etaria3_2022','9514','6','93','all','AM',[5,6,12,10,4],idade3)
 
 
