44379d08ffaf154ebb95997e2db81acf2074a385
afranio-viana <afranio.esquerdo.viana@gmail.com>
2025-11-05 20:58:56 -0400

    feat(dbt): cria o stg_pop_destino_lixo com o dbt

---
+++ dbt_dengue/models/staging/stg_pop_destino_lixo.sql
@@ -0,0 +1,20 @@
+{{config(matreialized="view")}}
+
+WITH raw_table AS (
+    SELECT * FROM {{source('raw','destino_lixo_2022')}}
+),
+
+clean AS (
+    SELECT
+        "ESTADO" AS estado,
+        "CODIGO"::text AS codigo_municipio,
+        REGEXP_REPLACE(TRIM(LOWER("MUNICIPIO")),'\s+',' ','g') AS municipio,
+        "DESTINO_DO_LIXO" AS destino_lixo,
+        COALESCE("VALOR",0)::int AS pop_destino_lixo,
+        "ANO"::text AS ano
+    FROM raw_table
+)
+
+SELECT
+    *
+FROM clean
