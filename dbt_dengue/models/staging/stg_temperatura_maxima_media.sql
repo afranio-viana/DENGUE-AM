513898129bf50daa54ff6b8e9b6ed35fabf6fe2d
afranio-viana <afranio.esquerdo.viana@gmail.com>
2025-11-05 23:33:53 -0400

    feat(dbt): cria o stg_temperatura_maxima_media com dbt

---
+++ dbt_dengue/models/staging/stg_temperatura_maxima_media.sql
@@ -0,0 +1,49 @@
+{{config(materialized='view')}}
+
+WITH raw_table AS (
+    SELECT * FROM {{source('raw','t2m_max_2022')}}
+),
+
+unpivot AS (
+    SELECT 
+        REGEXP_REPLACE(TRIM(LOWER("MUNICIPIO")),'\s+',' ','g') AS municipio,
+        key as mes,
+        value as temp_media,
+        t.*
+    FROM (
+        SELECT *, TO_JSONB(raw_table) AS jb
+        FROM raw_table
+    ) t
+    CROSS JOIN LATERAL(
+        SELECT * FROM JSONB_EACH_TEXT(t.jb - 'MUNICIPIO' - 'YEAR' - 'ANN' - 'PARAMETER')
+    ) AS j(key, value)
+),
+
+clean AS (
+    SELECT
+        municipio,
+        "ANN" AS temperatura_media_maxima_anual,
+        mes,
+        CASE UPPER(mes)
+            WHEN 'JAN' THEN "YEAR" || '01'
+            WHEN 'FEB' THEN "YEAR" || '02'
+            WHEN 'MAR' THEN "YEAR" || '03'
+            WHEN 'APR' THEN "YEAR" || '04'
+            WHEN 'MAY' THEN "YEAR" || '05'
+            WHEN 'JUN' THEN "YEAR" || '06'
+            WHEN 'JUL' THEN "YEAR" || '07'
+            WHEN 'AUG' THEN "YEAR" || '08'
+            WHEN 'SEP' THEN "YEAR" || '09'
+            WHEN 'OCT' THEN "YEAR" || '10'
+            WHEN 'NOV' THEN "YEAR" || '11'
+            WHEN 'DEC' THEN "YEAR" || '12'
+        END AS ano_mes,
+        COALESCE(temp_media,'0.0')::float AS temperatura_media_maxima_mensal,
+        "YEAR"::text AS ano
+    FROM unpivot
+)
+
+SELECT
+    *
+FROM clean
+ORDER BY municipio, ano_mes ASC
